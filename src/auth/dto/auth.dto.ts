d98884fc824ff1504fe2a1858f38a8bb0e2f0077
Bram Janssen <ibramsterdam@hotmail.com>
2022-08-21 22:45:23 +0200

    Password validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
@@ -7,5 +7,9 @@ export class AuthDto {
 
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+    message:
+      'Password must be minumum of eight characters, at least one letter and one number',
+  })
   password: string;
 }
